95f0f3227d3a175f8fe4e3915446184486c645ad
Joel E. Rego <joelerego@gmail.com>
2020-12-28 20:27:31 +0530

    Updates

---
+++ precache-manifest.de1d65e0a6e3e1d0c51ba543fd1a856b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258cc319bb83e717cee27b49909016a4",
+    "url": "/paywall/index.html"
+  },
+  {
+    "revision": "bdfe12537e393b0aae97",
+    "url": "/paywall/static/js/0.db72e3e9.chunk.js"
+  },
+  {
+    "revision": "95a62af98f1072a76262",
+    "url": "/paywall/static/js/1.e95ea0e3.chunk.js"
+  },
+  {
+    "revision": "27f8071fe335112b0678",
+    "url": "/paywall/static/js/10.fdbca30c.chunk.js"
+  },
+  {
+    "revision": "25d40c722d24a5d2864f",
+    "url": "/paywall/static/js/11.9381338b.chunk.js"
+  },
+  {
+    "revision": "661575caa174c180c3d2",
+    "url": "/paywall/static/js/12.2a765422.chunk.js"
+  },
+  {
+    "revision": "0d08c3e1001e6a6af931",
+    "url": "/paywall/static/js/13.8839b0bb.chunk.js"
+  },
+  {
+    "revision": "2ae8a3667fbe8475e4ba",
+    "url": "/paywall/static/js/14.c42e0a29.chunk.js"
+  },
+  {
+    "revision": "70094442f5f1c3d2e667",
+    "url": "/paywall/static/js/15.b0bf6e1d.chunk.js"
+  },
+  {
+    "revision": "5a78f634b23c10d87bc6",
+    "url": "/paywall/static/js/16.4ec6502d.chunk.js"
+  },
+  {
+    "revision": "429425bbaaa3500eee4b",
+    "url": "/paywall/static/js/17.dd9de487.chunk.js"
+  },
+  {
+    "revision": "10f7f168695b86170648",
+    "url": "/paywall/static/js/2.73beec13.chunk.js"
+  },
+  {
+    "revision": "7ac93cbad5dba8fab153",
+    "url": "/paywall/static/js/5.4a6e9755.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/paywall/static/js/5.4a6e9755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97308fd4a84a73882d90",
+    "url": "/paywall/static/js/6.2e3737a7.chunk.js"
+  },
+  {
+    "revision": "5d5a7472ddb3a9e10811a76a172a8e88",
+    "url": "/paywall/static/js/6.2e3737a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b57dee449e6ccf808ad1",
+    "url": "/paywall/static/js/7.d9b22405.chunk.js"
+  },
+  {
+    "revision": "9b5d0a39eea0e2b2a19b",
+    "url": "/paywall/static/js/8.33cb4627.chunk.js"
+  },
+  {
+    "revision": "b1dbe05fe3eac4bd193f",
+    "url": "/paywall/static/js/9.8463b1a7.chunk.js"
+  },
+  {
+    "revision": "194cef644afeb277a46a",
+    "url": "/paywall/static/js/main.f10af2e5.chunk.js"
+  },
+  {
+    "revision": "cccc486b4f4d66fb0f98",
+    "url": "/paywall/static/js/runtime-main.242a76da.js"
+  },
+  {
+    "revision": "188e0e531c8c950a48b71a9e490b4481",
+    "url": "/paywall/static/media/Hubble.188e0e53.ttf"
+  }
+]);
